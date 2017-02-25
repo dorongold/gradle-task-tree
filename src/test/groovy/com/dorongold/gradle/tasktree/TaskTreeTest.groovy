305a50f84d035b4030964625d2babb1b699c7af9
Doron Gold <dorongold@users.noreply.github.com>
2017-02-26 00:41:05 +0200

    Add tests
    tests for multiple gradle versions: 2.3-current.
    only tests minor versions. micro versions are not tested.
    i.e. the range of versions to test uses an increment of 0.1.

---
+++ src/test/groovy/com/dorongold/gradle/tasktree/TaskTreeTest.groovy
@@ -0,0 +1,97 @@
+package com.dorongold.gradle.tasktree
+
+import groovy.json.JsonSlurper
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.ClassRule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static org.gradle.testkit.runner.TaskOutcome.SKIPPED
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+/**
+ * Created by dorongold on 2/18/17.
+ */
+class TaskTreeTest extends Specification {
+
+    public static final String CURRENT_GRADLE_ENDPOINT = 'https://services.gradle.org/versions/current'
+    @ClassRule
+    @Shared
+    TemporaryFolder testProjectDir = new TemporaryFolder()
+    @Shared
+    File buildFile
+    @Shared
+    List<File> pluginClasspath
+    @Shared
+    List gradleVersions = []
+    @Shared
+    List gradleVersionsThatDontCaptureTestTasks = [2.3, 2.4]
+
+    def setupSpec() {
+        buildFile = testProjectDir.newFile('build.gradle')
+
+        def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.txt")
+        if (pluginClasspathResource == null) {
+            throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
+        }
+
+        pluginClasspath = pluginClasspathResource.readLines().collect { new File(it) }
+
+        def currentGradleJson = CURRENT_GRADLE_ENDPOINT.toURL().text
+        def currentGradleObject = new JsonSlurper().parseText(currentGradleJson)
+        def currentGradle = currentGradleObject.version
+        2.3.step((currentGradle as BigDecimal) + 0.1, 0.1) { gradleVersions << it }
+    }
+
+    def "running taskTree on the build task in every supported gradle version"() {
+        given:
+        def classpathString = pluginClasspath
+                .collect { it.absolutePath.replace('\\', '\\\\') } // escape backslashes in Windows paths
+                .collect { "'$it'" }
+                .join(", ")
+
+        buildFile << """
+            buildscript {
+                dependencies {
+                    classpath files($classpathString)
+                }
+            }
+            apply plugin: 'java'
+            apply plugin: 'com.dorongold.task-tree'
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('build', 'taskTree')
+                .withGradleVersion(gradleVersion as String)
+                .build()
+
+        then:
+        result.output.count('-- :processResources') == 3
+        result.output.count('-- :compileJava') == 3
+        if (!(gradleVersion in gradleVersionsThatDontCaptureTestTasks)) {
+            result.task(":taskTree").outcome == SUCCESS
+            result.task(":build").outcome == SKIPPED
+        }
+
+        when:
+        result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('build', 'taskTree', '--no-repeat')
+                .withGradleVersion(gradleVersion as String)
+                .build()
+
+        then:
+        result.output.count('-- :processResources') == 1
+        result.output.count('-- :compileJava') == 1
+        if (!(gradleVersion in gradleVersionsThatDontCaptureTestTasks)) {
+            result.task(":taskTree").outcome == SUCCESS
+            result.task(":build").outcome == SKIPPED
+        }
+
+        where:
+        gradleVersion << gradleVersions
+    }
+}
