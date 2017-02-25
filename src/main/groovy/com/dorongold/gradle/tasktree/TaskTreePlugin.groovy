6e339e80b6b8b99f8d23fa25323baa815bcd15fe
Doron Gold <dorongold@users.noreply.github.com>
2017-02-25 22:51:53 +0200

    Verify that the taskTree task name does not already exist

--- src/main/groovy/com/dorongold/gradle/tasktree/TaskTreePlugin.groovy
+++ src/main/groovy/com/dorongold/gradle/tasktree/TaskTreePlugin.groovy
@@ -3,7 +3,7 @@ package com.dorongold.gradle.tasktree
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.invocation.Gradle
+import org.gradle.api.plugins.PluginInstantiationException
 
 /**
  * User: dorongold
@@ -11,9 +11,13 @@ import org.gradle.api.invocation.Gradle
  */
 class TaskTreePlugin implements Plugin<Project> {
 
+
+    public static final String TASK_TREE_TASK_NAME = 'taskTree'
+
     void apply(Project project) {
+        verifyPluginNameAvailable(project)
         project.allprojects { p ->
-            p.task('taskTree', type: TaskTreeTask)
+            p.task(TASK_TREE_TASK_NAME, type: TaskTreeTask)
             p.gradle.taskGraph.whenReady {
                 if (project.gradle.taskGraph.allTasks.any { Task task -> task.class in TaskTreeTask }) {
                     p.tasks.each { Task task ->
@@ -23,6 +27,12 @@ class TaskTreePlugin implements Plugin<Project> {
                     }
                 }
             }
+        }
+    }
+
+    private void verifyPluginNameAvailable(Project project) {
+        if (project.tasks.findByName(TASK_TREE_TASK_NAME)) {
+            throw new PluginInstantiationException("Cannot instantiate plugin [taskTree] due to hijacked task name (${TASK_TREE_TASK_NAME})")
         }
     }
 
