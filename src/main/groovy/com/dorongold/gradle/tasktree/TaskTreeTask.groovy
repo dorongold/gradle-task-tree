d7cbef5013eb9402be75d3c899177a091a5d36e5
Doron Gold <dorongold@users.noreply.github.com>
2017-02-26 00:27:09 +0200

    One version to rule them all
    Remove import of StyledTextOutput. Instead use a dynamic
    groovy variable.
    This solves the issue of having different imports in
    for different gradle versions.
    
    Also refactor some more code to use groovy dynamic capabilities
    instead of using explicit Java reflection.

--- src/main/groovy/com/dorongold/gradle/tasktree/TaskTreeTask.groovy
+++ src/main/groovy/com/dorongold/gradle/tasktree/TaskTreeTask.groovy
@@ -1,31 +1,24 @@
 package com.dorongold.gradle.tasktree
 
-import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.execution.TaskExecutionGraph
 import org.gradle.api.internal.tasks.options.Option
 import org.gradle.api.tasks.diagnostics.AbstractReportTask
 import org.gradle.api.tasks.diagnostics.internal.ReportRenderer
 import org.gradle.api.tasks.diagnostics.internal.TextReportRenderer
-import org.gradle.execution.taskgraph.TaskDependencyGraph
 import org.gradle.execution.taskgraph.TaskExecutionPlan
 import org.gradle.execution.taskgraph.TaskInfo
 import org.gradle.initialization.BuildClientMetaData
 import org.gradle.internal.graph.GraphRenderer
-import org.gradle.internal.logging.text.StyledTextOutput
 import org.gradle.util.CollectionUtils
-
-import java.lang.reflect.Field
-
-import static org.gradle.internal.logging.text.StyledTextOutput.Style.*
 
 /**
  * User: dorongold
  * Date: 10/09/15
  */
 class TaskTreeTask extends AbstractReportTask {
-    private TextReportRenderer renderer = new TextReportRenderer();
-    private boolean noRepeat = false;
+    public TextReportRenderer renderer = new TextReportRenderer()
+    private boolean noRepeat = false
 
     @Override
     protected ReportRenderer getRenderer() {
@@ -35,106 +28,89 @@ class TaskTreeTask extends AbstractReportTask {
     @Override
     protected void generate(final Project project) throws IOException {
 
-        BuildClientMetaData metaData = getClientMetaData();
+        BuildClientMetaData metaData = getClientMetaData()
 
-        StyledTextOutput textOutput = getRenderer().getTextOutput();
+        // textOutput is injected and set into renderer by the parent abstract class before this method is called
+        // define textOutput as a dynamic type because it resides in different packages in different gradle versions
+        def textOutput = getRenderer().getTextOutput()
 
-        def TaskExecutionGraph g = project.gradle.taskGraph
-        // Access private variables of tasks graph
-        def TaskExecutionPlan tep = getTEP(g)
-        // Execution starts on these tasks
-        def Set<TaskInfo> entryTasks = getEntryTasks(tep)
-        // Already processed edges
+        TaskExecutionGraph executionGraph = project.gradle.taskGraph
+        // Getting a private field is possible thanks to groovy not honoring the private modifier
+        TaskExecutionPlan executionPlan = executionGraph.taskExecutionPlan
+        // Getting a private field is possible thanks to groovy not honoring the private modifier
+        Set<TaskInfo> entryTasks = executionPlan.entryTasks
         Set<TaskInfo> tasksOfCurrentProject = entryTasks.findAll { it.getTask().getProject() == project }
+
+        // take advantage of gradle's dynamic nature and get the Style enum (which has different FQNs in different gradle versions)
+        // from textOutput (which itself is of a dynamic type)
+        Class Style = textOutput.style.class
 
         tasksOfCurrentProject.findAll { !(it.task.class in TaskTreeTask) }.each {
             render(it, new GraphRenderer(textOutput), true, textOutput, true, new HashSet<Object>())
             if (it.dependencySuccessors.isEmpty()) {
-                textOutput.withStyle(Info).text("No task dependencies");
-                textOutput.println();
+                textOutput.withStyle(Style.Info).text("No task dependencies")
+                textOutput.println()
             }
             textOutput.println()
         }
 
         if (noRepeat) {
-            textOutput.println();
-            textOutput.text("(*) - subtree omitted (printed previously)");
-            textOutput.println();
+            textOutput.println()
+            textOutput.text("(*) - subtree omitted (printed previously)")
+            textOutput.println()
         }
 
-        textOutput.println();
-        textOutput.text("To see task dependency tree for a specific task, run ");
-        metaData.describeCommand(textOutput.withStyle(UserInput), String.format("<project-path>:<task> <project-path>:taskTree [--no-repeat]"));
-        textOutput.println();
+        textOutput.println()
+        textOutput.text("To see task dependency tree for a specific task, run ")
+        metaData.describeCommand(textOutput.withStyle(Style.UserInput), String.format("<project-path>:<task> <project-path>:taskTree [--no-repeat]"))
+        textOutput.println()
 
         textOutput.text("Executions of all tasks except for ")
-        textOutput.withStyle(UserInput).text('taskTree')
+        textOutput.withStyle(Style.UserInput).text('taskTree')
         textOutput.text(" will be skipped. They will be used for building the task graph only.")
-        textOutput.println();
-        textOutput.println();
+        textOutput.println()
+        textOutput.println()
         textOutput.text("Add ")
-        textOutput.withStyle(UserInput).text('--no-repeat')
+        textOutput.withStyle(Style.UserInput).text('--no-repeat')
         textOutput.text(" to prevent printing a subtree of the same task more than once.")
 
 
-        textOutput.println();
-        textOutput.println();
-        textOutput.text("For example, try running ");
-        Project exampleProject = project.getChildProjects().isEmpty() ? project : getChildren(project).get(0);
-        metaData.describeCommand(textOutput.withStyle(UserInput), exampleProject.absoluteProjectPath('build'), exampleProject.absoluteProjectPath('taskTree'));
-        textOutput.println();
+        textOutput.println()
+        textOutput.println()
+        textOutput.text("For example, try running ")
+        Project exampleProject = project.getChildProjects().isEmpty() ? project : getChildren(project).get(0)
+        metaData.describeCommand(textOutput.withStyle(Style.UserInput), exampleProject.absoluteProjectPath('build'), exampleProject.absoluteProjectPath('taskTree'))
+        textOutput.println()
     }
 
     @Option(option = "no-repeat", description = "prevent printing same subtree more than once")
-    public void setNoRepeat(boolean noRepeat) {
+    void setNoRepeat(boolean noRepeat) {
         this.noRepeat = noRepeat
     }
 
-    public boolean isNoRepeat() {
+    boolean isNoRepeat() {
         return noRepeat
     }
 
-    private TaskExecutionPlan getTEP(TaskExecutionGraph teg) {
-        Field f = teg.getClass().getDeclaredField("taskExecutionPlan")
-        f.setAccessible(true)
-        f.get(teg)
-    }
-
-    private Set<TaskInfo> getEntryTasks(TaskExecutionPlan tep) {
-        Field f = tep.getClass().getDeclaredField("entryTasks")
-        f.setAccessible(true)
-        Set<TaskInfo> entryTasks = f.get(tep)
-        entryTasks
-    }
-
-    private TaskDependencyGraph getTDG(TaskExecutionPlan tep) {
-        Field f2 = tep.getClass().getDeclaredField("graph")
-        f2.setAccessible(true)
-        f2.get(tep)
-    }
-
     void render(final TaskInfo entryTask, GraphRenderer renderer, boolean lastChild,
-                final StyledTextOutput textOutput, boolean isFirst, Set<Object> rendered) {
+                final textOutput, boolean isFirst, Set<Object> rendered) {
         final boolean taskSubtreeAlreadyPrinted = rendered.add(entryTask)
-        renderer.visit(new Action<StyledTextOutput>() {
-            public void execute(StyledTextOutput styledTextOutput) {
-                styledTextOutput.withStyle(isFirst ? Identifier : Normal).text(entryTask.task.path + ((!noRepeat || taskSubtreeAlreadyPrinted) ? "" : " *"));
-//                if (GUtil.isTrue(project.getDescription())) {
-//                    textOutput.withStyle(Description).format(" - %s", project.getDescription());
-//                }
-            }
-        }, lastChild);
+        renderer.visit({ styledTextOutput ->
+            Class Style = styledTextOutput.style.class
+            styledTextOutput.withStyle(isFirst ? Style.Identifier : Style.Normal)
+            styledTextOutput.text(entryTask.task.path + ((!noRepeat || taskSubtreeAlreadyPrinted) ? "" : " *"))
+        }, lastChild)
         if (!noRepeat || taskSubtreeAlreadyPrinted) {
-            renderer.startChildren();
+            renderer.startChildren()
             Set<TaskInfo> children = entryTask.dependencySuccessors
             children.eachWithIndex { TaskInfo child, int i ->
-                this.render(child, renderer, i == children.size() - 1, textOutput, false, rendered);
+                this.render(child, renderer, i == children.size() - 1, textOutput, false, rendered)
             }
-            renderer.completeChildren();
+            renderer.completeChildren()
         }
     }
 
     private List<Project> getChildren(Project project) {
-        return CollectionUtils.sort(project.getChildProjects().values());
+        return CollectionUtils.sort(project.getChildProjects().values())
     }
 }
