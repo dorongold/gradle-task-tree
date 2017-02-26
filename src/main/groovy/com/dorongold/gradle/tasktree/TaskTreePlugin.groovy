207755c107020819b7e7aecc5edc6c5f66b6710d
Doron Gold <dorongold@users.noreply.github.com>
2017-02-27 01:46:18 +0200

    Better functionality in multi-projects
    - Do not fail if the plugin is applied on allProjects.
       instead, if the taskTree task already exists on a project, skip.
    - Run taskTree only on the project on which the task is invoked
       and do not run on sub-projects. This is better aligned with how
       gradle help tasks, such as 'dependencies', work.

--- src/main/groovy/com/dorongold/gradle/tasktree/TaskTreePlugin.groovy
+++ src/main/groovy/com/dorongold/gradle/tasktree/TaskTreePlugin.groovy
@@ -3,7 +3,6 @@ package com.dorongold.gradle.tasktree
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.plugins.PluginInstantiationException
 
 /**
  * User: dorongold
@@ -15,9 +14,16 @@ class TaskTreePlugin implements Plugin<Project> {
     public static final String TASK_TREE_TASK_NAME = 'taskTree'
 
     void apply(Project project) {
-        verifyPluginNameAvailable(project)
         project.allprojects { p ->
-            p.task(TASK_TREE_TASK_NAME, type: TaskTreeTask)
+            if (p.tasks.findByName(TASK_TREE_TASK_NAME)) {
+                // Skip of this sub-project already has our task. This can happen for example if the plugin is applied on allProjects.
+                return
+            }
+            p.task(TASK_TREE_TASK_NAME, type: TaskTreeTask) {
+                // Run the task only for the current project and not for sub-projects.
+                // The DependencyReportTask and other help tasks do the same. See: org.gradle.api.plugins.HelpTasksPlugin
+                impliesSubProjects = true
+            }
             p.gradle.taskGraph.whenReady {
                 if (project.gradle.taskGraph.allTasks.any { Task task -> task.class in TaskTreeTask }) {
                     p.tasks.each { Task task ->
@@ -27,12 +33,6 @@ class TaskTreePlugin implements Plugin<Project> {
                     }
                 }
             }
-        }
-    }
-
-    private void verifyPluginNameAvailable(Project project) {
-        if (project.tasks.findByName(TASK_TREE_TASK_NAME)) {
-            throw new PluginInstantiationException("Cannot instantiate plugin [taskTree] due to hijacked task name (${TASK_TREE_TASK_NAME})")
         }
     }
 
